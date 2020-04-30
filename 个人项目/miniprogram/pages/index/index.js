377dfa264aa00d2ac4500212a4cab168c6c6bab7
wangxiya123 <jtxxwxy10@126.com>
2020-04-30 18:09:21 +0800

    Add files via upload

---
+++ 个人项目/miniprogram/pages/index/index.js
@@ -0,0 +1,83 @@
+Page({
+  data: {
+  region:["北京市","北京市","朝阳区"],
+  now:""
+  },
+ changeregion:function(e){
+   this.setData({region:e.detail.value}),
+   this.getweather()//更新天气
+ },
+
+ getweather:function(){
+   var that=this; //this不可以直接在wxAPI函数内部使用
+   wx.request({
+     url: 'https://free-api.heweather.net/s6/weather/now?',
+       data:{
+       location:that.data.region[1],
+       key:'d172176f1b3449f499756fc95ac31e46',
+       },
+       success:function(res){
+        //  console.log(res.data)
+        that.setData({now:res.data.HeWeather6[0].now})
+        }     
+
+    })
+  },
+    /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getweather()
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+ 
+})
